9edc3fb01dde677e116d19f324ebceed0c0b2e9b
Kirill Romanov <freibird22@gmail.com>
2022-08-03 19:13:29 +0300

    Cycle method added.

--- Task_25/Program.cs
+++ Task_25/Program.cs
@@ -2,8 +2,26 @@
 //3, 5 -> 243 (3⁵)
 //2, 4 -> 16
 
-Console.WriteLine("Enter A number ");
-int a = int.Parse(Console.ReadLine());
-Console.WriteLine("Enter B number ");
-int b = int.Parse(Console.ReadLine());
-Console.WriteLine("A in the B power = " + Math.Pow(a, b));
+//Console.WriteLine("Enter A number ");
+//int a = int.Parse(Console.ReadLine());
+//Console.WriteLine("Enter B number ");
+//int b = int.Parse(Console.ReadLine());
+//Console.WriteLine("A in the B power = " + Math.Pow(a, b));
+
+Console.Write("Enter A number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter B number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int Power(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    return result;
+}
+
+int power = Power(a, b);
+Console.WriteLine("A in the B power = " + power);
