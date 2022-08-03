9dca6facb76059575d590d6f4af9da39a127e260
Kirill Romanov <freibird22@gmail.com>
2022-08-03 14:37:31 +0300

    A in the B power.

---
+++ Task_25/Program.cs
@@ -0,0 +1,9 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+Console.WriteLine("Enter A number ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter B number ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("A in the B power = " + Math.Pow(a, b));
