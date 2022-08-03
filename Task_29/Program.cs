f15224b78dd051c3f43d49622ef7008a9ba06b5c
Kirill Romanov <freibird22@gmail.com>
2022-08-03 18:42:13 +0300

    8 elements array.

---
+++ Task_29/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+
+int size = 8;
+int[] array = new int[8];
+Random myRandom = new Random();
+Console.WriteLine("Eight elements array");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = myRandom.Next(0, 100);
+    Console.Write("{0}, ", array[i]);
+
+}
