7ad8a5a88dc8da2bd237423dfeb22efba4a6f265
Kirill Romanov <freibird22@gmail.com>
2022-08-03 16:04:19 +0300

    Sum of Digits of Number.

---
+++ Task_27/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+Console.WriteLine("Enter your number: ");
+int a = int.Parse(Console.ReadLine());
+
+int DigitalSum(int a)
+{
+    int counter = Convert.ToString(a).Length;
+    int digit = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++)
+    {
+        digit = a - a % 10;
+        result = result + (a - digit);
+        a = a / 10;
+    }
+    return result;
+}
+
+int digitalsum = DigitalSum(a);
+Console.WriteLine("Sum of digits = " + digitalsum);
