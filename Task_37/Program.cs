157e7d0c0e313049158c49d9fb2cb4622f28a1af
Kirill Romanov <freibird22@gmail.com>
2022-08-10 19:00:56 +0300

    Multiply pairs

---
+++ Task_37/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Задача 37: Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+[1 2 3 4 5] -> 5 8 3
+[6 7 3 6] -> 36 21*/
+int[] array = new int[] { 1, 2, 3, 4 };
+
+int[] result;
+
+if (array.Length % 2 == 0)
+{
+    result = new int[array.Length / 2];
+}
+else
+{
+    result = new int[array.Length / 2 + 1];
+}
+
+for (int i = 0, j = array.Length - 1; i < array.Length / 2; i++, j--)
+{
+    result[i] = array[i] * array[j];
+}
+
+if (array.Length % 2 != 0)
+{
+    result[result.Length - 1] = array[array.Length / 2];
+}
+
+for (int i = 0; i < result.Length; i++)
+{
+    Console.Write(result[i] + " ");
+}
+Console.WriteLine();
