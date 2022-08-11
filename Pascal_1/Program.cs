ff0a53e09d6f5801340cda27333d135a57df6a92
Kirill Romanov <freibird22@gmail.com>
2022-08-11 18:53:12 +0300

    Cross Point found

---
+++ Pascal_1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Enter b1: ");
+var b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter k1: ");
+var k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter b2: ");
+var b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter k2: ");
+var k2 = Convert.ToDouble(Console.ReadLine());
+
+
+var x = (b2 - b1) / (k1 - k2);
+var y = k1 * ((b2 - b1) / (k1 - k2)) + b1;
+
+x = Math.Round(x, 3);
+y = Math.Round(y, 3);
+
+Console.WriteLine($"Cross point: ({x}; {y})");
