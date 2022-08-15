43a89c8b7b71bcfe2c685392f7aa6e58da87c381
Kirill Romanov <freibird22@gmail.com>
2022-08-15 12:20:17 +0300

    Cross Point found.

--- Pascal_1/Program.cs
+++ Pascal_1/Program.cs
@@ -1,4 +1,9 @@
-﻿Console.Write("Enter b1: ");
+﻿/*Задача 43
+Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+Пример:
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
+
+Console.Write("Enter b1: ");
 var b1 = Convert.ToDouble(Console.ReadLine());
 Console.Write("Enter k1: ");
 var k1 = Convert.ToDouble(Console.ReadLine());
