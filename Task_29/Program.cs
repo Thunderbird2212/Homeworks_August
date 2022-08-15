3badbf6f03436a98370f8abf58d248d3d7666f71
Kirill Romanov <freibird22@gmail.com>
2022-08-15 12:24:22 +0300

    Eight elements array.

--- Task_29/Program.cs
+++ Task_29/Program.cs
@@ -2,7 +2,6 @@
 //1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 //6, 1, 33 -> [6, 1, 33]
 
-int size = 8;
 int[] array = new int[8];
 Random myRandom = new Random();
 Console.WriteLine("Eight elements array");
@@ -12,3 +11,4 @@ for (int i = 0; i < array.Length; i++)
     Console.Write("{0}, ", array[i]);
 
 }
+Console.WriteLine();
