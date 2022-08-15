7e00aaad788c0914588367bc0c42338c2b814b11
Kirill Romanov <freibird22@gmail.com>
2022-08-15 14:37:32 +0300

    Random double numbers

---
+++ Task_47/Program.cs
@@ -0,0 +1,43 @@
+﻿/*Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9*/
+
+Console.Write("Enter m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] array = new double[m, n];
+
+Console.WriteLine();
+
+void GetArray(double[,] array)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = new Random().NextDouble() * 20 - 10;
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            double arrayNumber = Math.Round(array[i, j], 1);
+            Console.Write(arrayNumber + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+GetArray(array);
+
+PrintArray(array);
+
+Console.WriteLine();
