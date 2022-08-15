26a2517bc2766d9ed0a88dfaf1bf17f91d45a55f
Kirill Romanov <freibird22@gmail.com>
2022-08-15 19:22:45 +0300

    Element Value.

---
+++ Task_50/Program.cs
@@ -0,0 +1,47 @@
+﻿/*Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет*/
+
+
+
+int[,] GetArray(int row, int col)
+{
+    int[,] array = new int[row, col];
+    Random random = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(0, 10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Enter amount of rows: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter amount of columns: ");
+int col = Convert.ToInt32(Console.ReadLine());
+
+PrintArray(GetArray(row, col));
+
+Console.Write("Enter Element position: ");
+int elementPosition = Convert.ToInt32(Console.ReadLine());
+
+
